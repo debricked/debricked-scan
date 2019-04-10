cd9fcc7673971048ff7f3eb1f1ff0001ec80142f
Oscar Reimer <oscar.reimer@debricked.com>
2019-04-10 15:10:36 +0200

    Minor fixes to GitLab CI integration

--- pipe/gitlab-ci.sh
+++ pipe/gitlab-ci.sh
@@ -3,7 +3,8 @@
 # CI script for integrating GitLab CI with Debricked. Automatically analyse your latest commits and pull requests for known vulnerabilities.
 #
 
-PRODUCT="${CI_MERGE_REQUEST_PROJECT_PATH}"
+PRODUCT="${CI_PROJECT_PATH}"
 RELEASE="${CI_COMMIT_SHA}"
+BASE_DIRECTORY=${BASE_DIRECTORY:=$CI_PROJECT_DIR}
 
 source "$(dirname "$0")/common.sh"
