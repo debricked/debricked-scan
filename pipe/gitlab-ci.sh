fa5b4e26ae7706dd3974576f35401f744e730fd8
Oscar Reimer <oscar.reimer@debricked.com>
2019-04-10 17:12:31 +0200

    Only run scan once in GitLab CI integration

--- pipe/gitlab-ci.sh
+++ pipe/gitlab-ci.sh
@@ -3,8 +3,12 @@
 # CI script for integrating GitLab CI with Debricked. Automatically analyse your latest commits and pull requests for known vulnerabilities.
 #
 
-PRODUCT="${CI_PROJECT_PATH}"
-RELEASE="${CI_COMMIT_SHA}"
-BASE_DIRECTORY=${BASE_DIRECTORY:=$CI_PROJECT_DIR}
+if [[ ! -f /ci_runned ]] ; then
+    PRODUCT="${CI_PROJECT_PATH}"
+    RELEASE="${CI_COMMIT_SHA}"
+    BASE_DIRECTORY=${BASE_DIRECTORY:=$CI_PROJECT_DIR}
 
-source "$(dirname "$0")/common.sh"
+    source "$(dirname "$0")/common.sh"
+fi
+
+[[ $CI ]] && touch /ci_runned
