0b3d4b924c9a3a367d2dc5c659d460877f093963
Gustav Svensson <gustav.svensson@debricked.com>
2019-09-24 10:14:04 +0200

    Add branch to gitlab-ci

--- pipe/gitlab-ci.sh
+++ pipe/gitlab-ci.sh
@@ -6,6 +6,7 @@
 if [[ ! -f /ci_runned ]] ; then
     PRODUCT="${CI_PROJECT_PATH}"
     RELEASE="${CI_COMMIT_SHA}"
+    BRANCH="${CI_COMMIT_REF_NAME}"
     BASE_DIRECTORY=${BASE_DIRECTORY:=$CI_PROJECT_DIR}
 
     source "$(dirname "$0")/common.sh"
