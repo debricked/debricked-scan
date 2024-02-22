a325e0eabbd81fbe31cf846326fb3918e4173fdf
Oscar Reimer <oscar.reimer@reimerbrothers.com>
2024-02-22 19:13:22 +0100

    Do not push to latest tag until legacy CLI has been deprecated

--- ci-scripts/release.sh
+++ ci-scripts/release.sh
@@ -29,7 +29,8 @@ echo "Build and push docker image..."
 echo ${DOCKERHUB_PASSWORD} | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
 docker build -t ${IMAGE} .
 docker tag ${IMAGE} ${IMAGE}:${new_version}
-docker push ${IMAGE}
+# TODO: Do not push to latest tag until legacy CLI has been deprecated
+#docker push ${IMAGE}
 docker push ${IMAGE}:${new_version}
 
 ##
